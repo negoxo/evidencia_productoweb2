0490a07d86cad196f84a23a7a028c9e36ed033f9
“Julian” <jalvarezgaviriajulian@cesde.net>
2024-05-25 20:07:01 -0500

    Actualizacion del proyecto

--- evidencia_productoweb2/src/App.js
+++ evidencia_productoweb2/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Img from './Img/valle-de-aburra-1.jpg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div 
+      className="App App-background"
+      style={{ backgroundImage: `url(${Img})` }} /* Establecer la imagen de fondo */
+    >
+      <h1 className="title">El Clima en el Valle de Aburrá</h1>
       <section className="columna1">
         <form>
           <label>Municipios del valle de aburra</label>
+          <br /><br />
           <select>
-            <option>Medellin</option>
+            <option>Medellín</option>
             <option>Envigado</option>
             <option>Sabaneta</option>
             <option>Caldas</option>
@@ -17,14 +25,20 @@ function App() {
             <option>Copacabana</option>
             <option>Barbosa</option>
             <option>Bello</option>
-            <option>Itagui</option>
+            <option>Itagüí</option>
           </select>
         </form>
       </section>
+      <br />
       <section className="columna2">
-
+        <form>
+          <label>Fecha que desea consultar</label>
+          <br /><br />
+          <select>
+            <option>Actual</option>
+          </select>
+        </form>
       </section>
-      
     </div>
   );
 }
