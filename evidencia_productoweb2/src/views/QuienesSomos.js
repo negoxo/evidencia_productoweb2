f7103fafe305bfdcc56f2b8edab67fe4ece392d7
Jose Daniel Ceballos Monsalve <josedceballos@gmail.com>
2024-06-18 22:24:10 -0500

    examen final refactor se organiza el router y se genera el consumo de la api de google

---
+++ evidencia_productoweb2/src/views/QuienesSomos.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+function QuienesSomos() {
+    return (
+        <main className="p-5">
+        <section className="relative flex flex-col md:flex-row items-center md:items-start mb-8">
+            <div className="md:w-1/2 p-5">
+                <h1 className="text-3xl font-bold ">Quiénes Somos</h1>
+                <p className="mt-4">Bienvenidos a nuestra página dedicada a los amantes del ciclismo y el turismo en Medellín y el Valle de Aburrá. Somos un equipo apasionado por el ciclismo y la tecnología, comprometidos en ofrecer una plataforma amigable y útil para todos los turistas que deseen explorar nuestra hermosa ciudad y sus alrededores en bicicleta.</p>
+                <p className="mt-4">Nuestra misión es proporcionar una experiencia única y placentera, combinando la belleza de los paisajes urbanos y rurales con la comodidad de la tecnología moderna. A través de nuestra página, puedes planificar tu ruta, consultar el clima y descubrir los mejores destinos para disfrutar de un recorrido inolvidable en bicicleta.</p>             
+
+            </div>
+            <div className="md:w-1/2 p-5 w-full h-auto rounded-lg shadow-lg">
+                <div className="bg-quienessomos-image w-full h-auto rounded-lg shadow-lg"></div>
+            </div>
+        </section>
+
+    </main>
+    );
+  }
+  
+  export default QuienesSomos;
--- evidencia_productoweb2/tailwind.config.js
+++ evidencia_productoweb2/tailwind.config.js
@@ -4,7 +4,8 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage:{
-        'logo-image': "url('/src/Img/WhatsApp Image 2024-06-17 at 9.17.53 AM.jpeg')"
+        'logo-image':"url('/src/Img/WhatsApp Image 2024-06-17 at 9.17.53 AM.jpeg')",
+        'mision-image':"url('/src/Img/Fietsvakanties zijn de afgelopen twee jaar flink populairder geworden_.jfif')",'vision-image':"url('/src/Img/AR aprova recomendações para o incentivo à mobilidade sustentável, nomeadamente à utilização de bicicletas.jfif')",'quienessomos-image':"url('/src/Img/Ciudad.jfif')"
       }
     },
   },
